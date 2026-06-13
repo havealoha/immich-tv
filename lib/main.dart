80f50889fb29e059e0f702208c34f03f6efe9a5e
Khondakar Afridi <afridi.khondakar@gmail.com>
2026-06-13 08:28:54 +0600

    Wire Firebase core into configured platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final imageCache = PaintingBinding.instance.imageCache;
   imageCache.maximumSize = 72;
   imageCache.maximumSizeBytes = 64 << 20;
+  await _initializeFirebaseIfSupported();
   await WakelockPlus.enable();
   runImmichTvApp();
+}
+
+Future<void> _initializeFirebaseIfSupported() async {
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.linux) {
+    return;
+  }
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_secure_storage_macos
 import package_info_plus
 import shared_preferences_foundation
@@ -12,6 +13,7 @@ import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
